13051319f9a10ce05b3aae94c2ab12436645ebfa
tylerthayn <tylerthayn@hotmail.com>
2021-12-31 22:25:45 -0700

    '.'

--- src/Uuid.js
+++ src/Uuid.js
@@ -1,11 +1,13 @@
 require('./global')
+require('./Define')
 
-global.Uuid = () => {
+Define(global, 'Uuid', () => {
 	let d = Date.now()
 	return `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, c => {
 		const r = (d + Math.random() * 16) % 16 | 0
 		d = Math.floor(d / 16)
 		return (c == `x` ? r : (r & 0x3 | 0x8)).toString(16)
 	})
-}
+})
+
 
